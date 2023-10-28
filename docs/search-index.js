d12f4c361d43e1a841f4c1161015105b2f8f3e52
VenerisAsgard <firstbf@outlook.com>
2023-10-28 18:29:03 +0300

    Add files via upload

---
+++ docs/search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = JSON.parse('{\
+"os":{"doc":"","t":"AFFFFF","n":["os","getcwd","listdir","name","path_join","path_parent"],"q":[[0,"os"],[1,"os::os"]],"d":["","Получение рабочей директории","Список файлов и директорий в …","Имя ОС","Соеденение путей PathBuf + [Путь 1, …","Получение родительской папки …"],"i":[0,0,0,0,0,0],"f":[0,[[],1],[1,[[2,[1]]]],[[],3],[[1,[2,[4]]],1],[1,1]],"c":[],"p":[[3,"PathBuf"],[3,"Vec"],[3,"String"],[15,"str"]]}\
+}');
+if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
+if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
